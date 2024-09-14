4e317eb0cfdca4ad000c4c80b4e693b579e5e8b1
GamingHackintosh <sawaretev@gmail.com>
2024-09-14 19:04:40 +1000

    upd. branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Hero from './components/Hero';
+import Hero from './components/Hero/Hero';
+import Build from './components/create-build/Build';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+
+      <Build />
     </div>
   );
 }
